4c280e5ca13a2c4f0e7b2abdbdfdb1462cfdc4f5
Mikhail B <jamescole2035@inbox.ru>
2024-07-03 15:08:20 +0500

    переписал

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -2,14 +2,15 @@ import './Profile.css'
 import photo from "../../images/bms.jpeg"
 
 function Profile(props) {
+  const { name, mail, tg } = props; 
   return (
     <>
       <div className="profile">
         <img className="profile__image" src={photo} alt="моя фотография" />
         <div className="profile__info">
-            <p className="profile__name">{props.name}</p>
-            <p className="profile__mail">{props.mail}</p>
-            <p className="profile__tg">{props.tg}</p>   
+            <p className="profile__name">{name}</p>
+            <p className="profile__mail">{mail}</p>
+            <p className="profile__tg">{tg}</p>   
         </div>             
       </div>      
     </>
